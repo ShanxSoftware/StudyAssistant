a730af11b65c46a09f19bbe4cf7302089d00ff9c
Jakob Shanks <jakob@shanx.com.au>
2026-03-09 08:51:40 +1000

    Initial Structure Created

---
+++ src/study_assistant/__main__.py
@@ -0,0 +1,5 @@
+from study_assistant.ui.console import main as console_main
+# from study_assistant.ui.gui import main as gui_main
+
+if __name__ == "__main__":
+    console_main()  # switch to gui_main() once ready
